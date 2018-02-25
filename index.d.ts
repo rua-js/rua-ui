986f74ad70a08857e8647f5e106c69d827ee049e
copydog <775150@qq.com>
2018-02-25 16:10:28 +0800

    + d.ts

---
+++ index.d.ts
@@ -0,0 +1,3 @@
+import { Placeholder, FluidPlaceholder, SquarePlaceholder } from './lib/Placeholders/index';
+import { PaddedView, SpaceBetweenView, TouchableView } from './lib/Views/index';
+export { Placeholder, FluidPlaceholder, SquarePlaceholder, PaddedView, SpaceBetweenView, TouchableView };
