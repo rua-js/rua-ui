f9369a43cf666365e5c16dd06fd4cc58d9e2c8f8
copydog <775150@qq.com>
2018-05-10 18:36:50 +0800

    finish IconText

--- src/Views/IconText.tsx
+++ src/Views/IconText.tsx
@@ -1,26 +1,81 @@
 import * as React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, TextStyle, View } from 'react-native'
 import { TouchableView } from '../Internals'
+import { Text } from '../Primitives'
+import { Space } from '../Elements'
+// material
+import { default as EntypoIcon } from 'react-native-vector-icons/Entypo'
+import { default as EvilIconsIcon } from 'react-native-vector-icons/EvilIcons'
+import { default as FeatherIcon } from 'react-native-vector-icons/Feather'
+import { default as FontAwesomeIcon } from 'react-native-vector-icons/FontAwesome'
+import { default as FoundationIcon } from 'react-native-vector-icons/Foundation'
+import { default as IoniconsIcon } from 'react-native-vector-icons/Ionicons'
+import { default as MaterialIcon } from 'react-native-vector-icons/MaterialCommunityIcons'
+import { default as OcticonsIcon } from 'react-native-vector-icons/Octicons'
+import { default as ZocialIcon } from 'react-native-vector-icons/Zocial'
+import { default as SimpleLineIconsIcon } from 'react-native-vector-icons/SimpleLineIcons'
+
+const iconComponents = {
+  entypo: EntypoIcon,
+  evil: EvilIconsIcon,
+  feather: FeatherIcon,
+  fontAwesome: FontAwesomeIcon,
+  foundation: FoundationIcon,
+  ionic: IoniconsIcon,
+  material: MaterialIcon,
+  octicons: OcticonsIcon,
+  zocial: ZocialIcon,
+  simpleLine: SimpleLineIconsIcon,
+}
 
 class IconText extends React.Component<Props, never>
 {
 
   static defaultProps = {
-    gutter: 5,
+    space: 5,
+    textColor: '#333',
+    iconSet: 'material'
   }
 
   render()
   {
-    const { icon, text, gutter, ...restProps } = this.props
+    const {
+      space,
+      icon,
+      iconSet,
+      iconColor,
+      iconSize,
+      iconStyle,
+      text,
+      textColor,
+      textSize,
+      textFamily,
+      textDecoration,
+      textWeight,
+      textLine,
+      ...restProps
+    } = this.props
+
+    const Icon = iconComponents[iconSet]
 
     const inner = (
       <View style={styles.container}>
         <View style={styles.iconText}>
-          {icon}
-          <View style={[styles.iconTextGutter, { height: gutter }]} />
-          <Text style={styles.iconTextText}>
-            {text}
-          </Text>
+          <Icon
+            name={icon}
+            color={iconColor}
+            size={iconSize}
+            style={iconStyle}
+          />
+          <Space size={space} />
+          <Text
+            color={textColor}
+            size={textSize}
+            family={textFamily}
+            decoration={textDecoration}
+            weight={textWeight}
+            line={textLine}
+          >{text}</Text>
         </View>
       </View>
     )
@@ -52,19 +107,29 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-  iconTextGutter: {
-    width: '100%',
-  },
-  iconTextText: {
-    color: '#333'
-  },
 })
 
 interface Props
 {
-  icon: React.ReactNode
-  text: string | number
-  gutter?: number
+  // Icon
+  icon?: string
+  iconSet?: string
+  iconStyle: any
+  iconColor: string
+  iconSize: number
+  // Text
+  text: string
+  textSize?: number
+  textWeight?: 'normal' | 'bold' | '100' | '200' | '300' | '400' | '500' | '600' | '700' | '800' | '900'
+  textAlign?: 'auto' | 'left' | 'right' | 'center' | 'justify'
+  textColor?: string
+  textLine?: 'none' | 'underline' | 'line-through' | 'underline line-through'
+  textStyle?: TextStyle
+  textDecoration?: 'none' | 'italic'
+  textLineHeight?: number
+  textFamily?: string
+  // space
+  space?: number
   // Touchable
   onPress?: Function
   onLongPress?: Function
