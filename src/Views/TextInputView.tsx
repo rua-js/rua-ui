7a17d97c91cd95dd749e31e90b70bef29047c463
copydog <775150@qq.com>
2018-05-19 16:10:21 +0800

    finish TextInputView

---
+++ src/Views/TextInputView.tsx
@@ -0,0 +1,42 @@
+import * as React from 'react'
+import {
+  StyleProp,
+  TextProps as RNTextProps,
+  TextStyle as RNTextStyle,
+  ViewStyle as RNViewStyle,
+} from 'react-native'
+import { TextInput, View, ViewProps } from '../Primitives'
+
+class TextInputView extends React.Component<TextInputViewProps, never>
+{
+  render()
+  {
+    const {
+      containerViewStyle,
+      containerViewProps,
+      textInputStyle,
+      textInputProps,
+    } = this.props
+    return (
+      <View
+        style={containerViewStyle}
+        {...containerViewProps}
+      >
+        <TextInput
+          style={textInputStyle}
+          {...textInputProps}
+        />
+      </View>
+    )
+  }
+}
+
+export interface TextInputViewProps
+{
+  containerViewStyle?: StyleProp<RNViewStyle>
+  containerViewProps?: ViewProps
+  textInputStyle?: StyleProp<RNTextStyle>
+  textInputProps?: RNTextProps
+}
+
+export default TextInputView
