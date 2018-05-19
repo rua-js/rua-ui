9675d61f953b5aa99375efaf5fff6af5f7f62e1d
copydog <775150@qq.com>
2018-05-19 16:28:35 +0800

    upgrade Text

--- src/Primitives/Text.tsx
+++ src/Primitives/Text.tsx
@@ -1,62 +1,20 @@
 import * as React from 'react'
-import {
-  Text as RNText,
-  TextProperties,
-  TextStyle,
-  StyleSheet,
-} from 'react-native'
+import { StyleSheet, Text as RNText, TextProps as RNTextProps, } from 'react-native'
 
 class Text extends React.Component<TextProps, never>
 {
-
-  /**
-   * Synchronize style between Android and iOS
-   *
-   * @type {object}
-   */
-  static defaultProps = {
-    style: {},
-  }
-
-  /**
-   * Render function
-   *
-   * @returns {any}
-   */
   render()
   {
-    // Pick out NONE-RN Text property
     const {
       children,
-      color,
       style,
-      lineHeight,
-      backgroundColor,
-      family: fontFamily,
-      line: textDecorationLine,
-      size: fontSize,
-      weight: fontWeight,
-      align: textAlign,
-      ...rest
+      ...restProps
     } = this.props
 
-    // Compute text style
-    const computedTextStyle = {
-      color,
-      fontWeight,
-      fontSize,
-      textAlign,
-      textDecorationLine,
-      lineHeight,
-      fontFamily,
-      backgroundColor,
-    }
-
-    // Return
     return (
       <RNText
-        style={[styles.el, computedTextStyle, style]}
-        {...rest}
+        style={[styles.textDefault, style]}
+        {...restProps}
       >
         {children}
       </RNText>
@@ -65,27 +23,13 @@ class Text extends React.Component<TextProps, never>
 }
 
 const styles = StyleSheet.create({
-  el: {
+  textDefault: {
     color: '#1b1b1b',
   },
 })
 
-interface TextProps extends TextProperties
+export interface TextProps extends RNTextProps
 {
-  size?: number
-  weight?: 'normal' | 'bold' | '100' | '200' | '300' | '400' | '500' | '600' | '700' | '800' | '900'
-  align?: 'auto' | 'left' | 'right' | 'center' | 'justify'
-  color?: string
-  line?: 'none' | 'underline' | 'line-through' | 'underline line-through'
-  style?: TextStyle
-  decoration?: 'none' | 'italic'
-  lineHeight?: number
-  family?: string
-  backgroundColor?: string
 }
 
 export default Text
-
-export {
-  TextProps,
-}
