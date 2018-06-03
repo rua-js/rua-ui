d6fc47b1a8bc4a20630013b21a23d16f51edbf69
copydog <775150@qq.com>
2018-06-03 17:44:00 +0800

    update index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -1,6 +1,30 @@
-import { Border, Button, Container, Space, Icon, Divider, Avatar } from './lib/Elements';
-import { Text, View, TextInput, Image, ScrollView } from './lib/Primitives';
-import { FluidPlaceholder, Placeholder, SquarePlaceholder } from './lib/Placeholders';
-import { Card, IconText, TextView, ImageView, Statistic, TextInputView } from './lib/Views';
-import { Picker } from './lib/DataEntries';
-export { Container, Space, Button, Border, Icon, Divider, Avatar, Text, View, Image, ScrollView, TextInput, Placeholder, FluidPlaceholder, SquarePlaceholder, Card, IconText, TextView, ImageView, Statistic, TextInputView, Picker };
+import { Avatar, Border, Button, Container, Divider, Icon, Space } from './lib/Elements'
+import { Image, ScrollView, Text, TextInput, View } from './lib/Primitives'
+import { FluidPlaceholder, Placeholder, SquarePlaceholder } from './lib/Placeholders'
+import { Card, IconText, ImageView, Statistic, TextInputView, TextView } from './lib/Views'
+import { Picker } from './lib/DataEntries'
+
+export {
+  Container,
+  Space,
+  Button,
+  Border,
+  Icon,
+  Divider,
+  Avatar,
+  Text,
+  View,
+  Image,
+  ScrollView,
+  TextInput,
+  Placeholder,
+  FluidPlaceholder,
+  SquarePlaceholder,
+  Card,
+  IconText,
+  TextView,
+  ImageView,
+  Statistic,
+  TextInputView,
+  Picker
+}
