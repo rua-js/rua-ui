167f33c13fca44d11ac342139f180d1afd2d14d6
copydog <775150@qq.com>
2018-07-05 21:00:26 +0800

    refactor: move Placeholder and merge all placeholder into one Placeholder

---
+++ src/Helpers/Placeholder.tsx
@@ -0,0 +1,40 @@
+import * as React from 'react'
+import { StyleSheet, View } from 'react-native'
+
+export default class Placeholder extends React.Component<PlaceholderProps, never>
+{
+
+  static defaultProps = {
+    color: 'red',
+    size: 100,
+  }
+
+  render()
+  {
+    const { height, width, color, size } = this.props
+
+    const $height = height || size
+    const $width = width || size
+
+    return (
+      <View
+        style={{
+          height: $height,
+          width: $width,
+          backgroundColor: color
+        }}
+      />
+    )
+  }
+}
+
+export interface PlaceholderProps
+{
+  height?: number | string
+
+  width?: number | string
+
+  color?: string
+
+  size?: number
+}
