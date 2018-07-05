c799515384cb10d5f2c3def2d6a83fcbed406b05
copydog <775150@qq.com>
2018-07-05 21:00:42 +0800

    feat(Repeat.tsx): add Repeat component

---
+++ src/Helpers/Repeat.tsx
@@ -0,0 +1,23 @@
+import * as React from 'react'
+
+export default class Repeat extends React.Component<RepeatProps, never>
+{
+  render()
+  {
+    const { count, children } = this.props
+
+    const returnValue = []
+
+    for (let i = 0; i < count; i += 1 )
+    {
+      returnValue.push(children)
+    }
+
+    return returnValue
+  }
+}
+
+export interface RepeatProps
+{
+  count: number
+}
