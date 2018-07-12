e857ff4b4f380a33a6b5d1fe913c896c37f4651b
copydog <775150@qq.com>
2018-07-12 22:54:37 +0800

    build: hit 0.9.4

--- index.d.ts
+++ index.d.ts
@@ -1,5 +1,6 @@
-export { Border, Button, Container, Space, Icon, Divider, Avatar } from './lib/Elements';
-export { Text, TextProps, View, ViewProps, TextInput, TextInputProps, Image, ScrollView } from './lib/Primitives';
-export { Card, IconText, TextView, ImageView, Statistic, TextInputView, Center, Column, Row } from './lib/Views';
-export { Picker } from './lib/DataEntries';
-export { Repeat, PlaceHolder } from './lib/Helpers';
+export { Border, Button, Container, Space, Icon, Divider, Avatar } from './lib/elements';
+export { Text, TextProps, View, ViewProps, TextInput, TextInputProps, Image, ScrollView } from './lib/primitives';
+export { Card, IconText, TextView, ImageView, Statistic, TextInputView, Center, Column, Row } from './lib/views';
+export { Picker } from './lib/data-entry';
+export { Repeat, PlaceHolder } from './lib/helpers';
+export { Fluid } from './lib/layouts';
