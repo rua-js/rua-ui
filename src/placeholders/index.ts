210e03d691918e7237f027ddfa08e3d0dd506294
copydog <775150@qq.com>
2018-07-28 09:56:25 +0800

    refactor: drop FluidPlaceholder and SquarePlaceholder

--- src/placeholders/index.ts
+++ src/placeholders/index.ts
@@ -1,9 +1,5 @@
 import Placeholder from './Placeholder'
-import FluidPlaceholder from './FluidPlaceholder'
-import SquarePlaceholder from './SquarePlaceholder'
 
 export {
   Placeholder,
-  FluidPlaceholder,
-  SquarePlaceholder,
 }
